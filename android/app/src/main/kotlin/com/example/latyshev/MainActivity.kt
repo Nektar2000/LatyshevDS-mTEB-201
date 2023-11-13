f23b6e2915c693e19dcf0a238b62c1fa132da81d
Nektar2000 <136623940+Nektar2000@users.noreply.github.com>
2023-11-13 18:16:30 +0700

    Final Project

---
+++ android/app/src/main/kotlin/com/example/latyshev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latyshev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+class AppConstants{
+  static String appName = " Заказы фирмы разработки ПО, Латышев Данила мТЭБ201 ";
+}
